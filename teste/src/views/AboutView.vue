6b66d568b0587c3765cc2c20913b2e8dc21ee2dc
zgbsss <99218597+zgbsss@users.noreply.github.com>
2022-10-13 15:16:52 -0300

    Add files via upload

---
+++ teste/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      token: "dddd",
+    };
+  },
+  methods: {
+    getHashParams() {
+      var hashParams = {};
+      var e,
+        r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+      e = r.exec(q);
+      while (e) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+        e = r.exec(q);
+      }
+      return hashParams;
+    },
+  },
+  created() {
+    this.token = this.$route.query["code"];
+    console.log(this.getHashParams(this.token));
+  },
+};
+</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h2>{{ token }}</h2>
+  </div>
+</template>
+
+<style></style>
---
+++ teste/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <button><a href="http://localhost:8888">logar</a></button>
+  </main>
+</template>
